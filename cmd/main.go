787d87a320f256bebe33327d43b567099d2de72f
minh.vo <dev.minhvo@gmail.com>
2024-10-11 17:19:56 +0700

    feat: getting some basic hardware info

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/minhvhd/go-monitor/internal/hardware"
+)
 
+func main() {
+	system, err := hardware.GetSystem()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	cpu, err := hardware.GetCPU()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	disk, err := hardware.GetDisk()
+
+	fmt.Println(system)
+	fmt.Println(cpu)
+	fmt.Println(disk)
 }
--- internal/hardware/hardware.go
+++ internal/hardware/hardware.go
@@ -1,1 +1,57 @@
 package hardware
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/disk"
+	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
+func GetSystem() (string, error) {
+	runtimeOS := runtime.GOOS
+
+	memStat, err := mem.VirtualMemory()
+	if err != nil {
+		return "", err
+	}
+
+	hostStat, err := host.Info()
+	if err != nil {
+		return "", err
+	}
+
+	output := fmt.Sprintf(
+		"OS: %s\nHost Name: %s\nTotal Memory: %d\nUsed Memory: %d\n",
+		runtimeOS, hostStat.Hostname, memStat.Total, memStat.Used)
+
+	return output, nil
+}
+
+func GetCPU() (string, error) {
+	cpuStat, err := cpu.Info()
+	if err != nil {
+		return "", err
+	}
+
+	persentage, err := cpu.Percent(0, false)
+	if err != nil {
+		return "", err
+	}
+
+	output := fmt.Sprintf("CPU: %s\nCPU Usage: %.2f%%\n", cpuStat[0].ModelName, persentage[0])
+
+	return output, nil
+}
+
+func GetDisk() (string, error) {
+	diskStat, err := disk.Usage("/")
+	if err != nil {
+		return "", err
+	}
+
+	output := fmt.Sprintf("Total Disk Space: %d\nDisk Usage: %.2fGB\n Free Disk Space: %d", diskStat.Total, diskStat.UsedPercent, diskStat.Free)
+	return output, nil
+}
